90464462c75f084b87690ac13bee82be83644f27
Ryan <rjkviegas@gmail.com>
2020-06-29 10:36:09 +0000

    updated spec/fizzbuzz_spec.rb with 3 more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,5 +4,14 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "fizz" when passed 33' do
+    expect(fizzbuzz(33)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed 25' do
+    expect(fizzbuzz(25)).to eq 'buzz'
+  end
+  its 'returns "fizzbuzz" when passed 30' do
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+  end
 end
 
