56ca446d491b4a2c807b24f65829f0f73a1f378a
Ryan <rjkviegas@gmail.com>
2020-06-29 10:24:28 +0000

    updated ./lib/fizzbuzz.rb file

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,13 @@
 def fizzbuzz(number)
-  'fizz'
+  number = number.to_i
+  if number % 3 == 0 && number % 5 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz' 
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
 end
 
